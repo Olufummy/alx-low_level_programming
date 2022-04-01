231a4c66fe22a8b2946671ff03c01ae9af1d365a
Olufummy <Adeboyejoisaac18@gmail,com>
2022-04-02 00:35:41 +0100

    0x09. C-STATIC_LIBARIES

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string of characters
+ *@str: pointer to a string
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{;
+
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
+© 2022 GitHub
